2f1ce860a1cafeb2a32d3af689d7fd544a6a96ac
jvican <jorgevc@fastmail.es>
2018-04-08 23:30:38 +0200

    Move the state to common

--- runners/zinc/src/main/scala/annex/AnxRunner.scala
+++ runners/zinc/src/main/scala/annex/AnxRunner.scala
@@ -52,6 +52,7 @@ object AnxRunner {
   val toAbsoluteFile: String => File = toFile andThen toAbsolute
 
   def main(options: Options): Int = {
+    println("HAHAHAHAHHAHA")
     Files.createDirectories(Paths.get(options.outputDir))
 
     val scalaInstance = AnxScalaInstance(
@@ -322,26 +323,4 @@ final class AnxLogger extends Logger {
   def trace(err: Supplier[Throwable]): Unit =
     println(s"[trace]: ${err.get.getMessage}")
 
-}
-
-// tiny state monad
-
-final case class State[S, A](run: S => (S, A)) {
-
-  def map[B](f: A => B): State[S, B] =
-    new State(s => {
-      val res = run(s)
-      (res._1, f(res._2))
-    })
-
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    new State(s => {
-      val res = run(s)
-      f(res._2).run(res._1)
-    })
-
-}
-
-object State {
-  def pure[S, A](a: A): State[S, A] = State(s => (s, a))
 }
